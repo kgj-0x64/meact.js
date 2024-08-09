15cb24bb92d4666dfa90f11dd8f48efc29bde203
Kumar Gaurav <kumargaurav.0x64@gmail.com>
2024-08-09 17:41:58 +0530

    JSX syntax working with Babel and prior meact example

---
+++ src/19-react-sim-jsx/README.md
@@ -0,0 +1,59 @@
+# JSX Syntax
+
+## Transform
+
+React supports JSX syntax through transpilation using tools like Babel or TypeScript. To support JSX syntax in your custom UI library, you'll need to set up a build process that transforms JSX into calls to your `createElement` function.
+
+### Babel
+
+Here's how you can do it:
+
+1. Set up a transpiler:
+   You'll need to use a transpiler like Babel to transform JSX syntax into function calls. Babel has a plugin specifically for this purpose.
+
+2. Configure the JSX transform:
+   You'll need to tell Babel to use your custom `createElement` function instead of React's.
+
+Here's a step-by-step guide to set this up:
+
+1. Install necessary dependencies:
+
+   ```bash
+   npm install --save-dev @babel/core @babel/cli @babel/preset-env @babel/plugin-transform-react-jsx
+   ```
+
+2. Create a Babel configuration file (`.babelrc` or `babel.config.js`) in your project root:
+
+   ```json
+   {
+     "presets": ["@babel/preset-env"],
+     "plugins": [
+       [
+         "@babel/plugin-transform-react-jsx",
+         {
+           "pragma": "createElement", // Use your function name here
+           "pragmaFrag": "'fragment'" // Use your fragment implementation if you have one
+         }
+       ]
+     ]
+   }
+   ```
+
+3. Update your code to use JSX syntax
+
+4. Set up your build process:
+   You'll need to run your code through Babel before executing it. You can do this in several ways:
+   - Use Babel CLI directly
+   - Integrate with a bundler like webpack or Rollup
+
+For example, using Babel CLI:
+
+```bash
+npx babel src --out-dir jsx-output
+```
+
+This will transpile your JSX files in the `src` directory and output the result to the `jsx-output` directory.
+
+Remember, you'll need to ensure that your `createElement` function and other custom hooks (`useState`, `useMemo`, etc.) are in scope where they're used. You might want to create a single entry point that exports all these functions, similar to how React does it.
+
+By following these steps, you should be able to use JSX syntax with your custom UI library. The Babel plugin will transform the JSX into calls to your `createElement` function, allowing you to write more readable and React-like code while still using your own implementation under the hood.
---
+++ src/19-react-sim-jsx/jsx-output/components.js
@@ -0,0 +1,157 @@
+"use strict";
+
+function _slicedToArray(r, e) {
+  return (
+    _arrayWithHoles(r) ||
+    _iterableToArrayLimit(r, e) ||
+    _unsupportedIterableToArray(r, e) ||
+    _nonIterableRest()
+  );
+}
+function _nonIterableRest() {
+  throw new TypeError(
+    "Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."
+  );
+}
+function _unsupportedIterableToArray(r, a) {
+  if (r) {
+    if ("string" == typeof r) return _arrayLikeToArray(r, a);
+    var t = {}.toString.call(r).slice(8, -1);
+    return (
+      "Object" === t && r.constructor && (t = r.constructor.name),
+      "Map" === t || "Set" === t
+        ? Array.from(r)
+        : "Arguments" === t ||
+          /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t)
+        ? _arrayLikeToArray(r, a)
+        : void 0
+    );
+  }
+}
+function _arrayLikeToArray(r, a) {
+  (null == a || a > r.length) && (a = r.length);
+  for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e];
+  return n;
+}
+function _iterableToArrayLimit(r, l) {
+  var t =
+    null == r
+      ? null
+      : ("undefined" != typeof Symbol && r[Symbol.iterator]) || r["@@iterator"];
+  if (null != t) {
+    var e,
+      n,
+      i,
+      u,
+      a = [],
+      f = !0,
+      o = !1;
+    try {
+      if (((i = (t = t.call(r)).next), 0 === l)) {
+        if (Object(t) !== t) return;
+        f = !1;
+      } else
+        for (
+          ;
+          !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l);
+          f = !0
+        );
+    } catch (r) {
+      (o = !0), (n = r);
+    } finally {
+      try {
+        if (!f && null != t["return"] && ((u = t["return"]()), Object(u) !== u))
+          return;
+      } finally {
+        if (o) throw n;
+      }
+    }
+    return a;
+  }
+}
+function _arrayWithHoles(r) {
+  if (Array.isArray(r)) return r;
+}
+// import Meact, { useState, useMemo, memo } from "meact";
+
+function GreetingApp() {
+  var _useState = useState(""),
+    _useState2 = _slicedToArray(_useState, 2),
+    name = _useState2[0],
+    setName = _useState2[1];
+  var _useState3 = useState(""),
+    _useState4 = _slicedToArray(_useState3, 2),
+    address = _useState4[0],
+    setAddress = _useState4[1];
+  var memoizedSetName = useMemo(
+    function () {
+      return function (e) {
+        return setName(e.target.value);
+      };
+    },
+    [name]
+  );
+  var memoizedSetAddress = useMemo(
+    function () {
+      return function (e) {
+        return setAddress(e.target.value);
+      };
+    },
+    [address]
+  );
+  return createElement(
+    "div",
+    null,
+    createElement(
+      "label",
+      null,
+      "Name: ",
+      createElement("input", {
+        value: name,
+        onChange: memoizedSetName,
+      })
+    ),
+    createElement(
+      "label",
+      null,
+      "Address: ",
+      createElement("input", {
+        value: address,
+        onChange: memoizedSetAddress,
+      })
+    ),
+    createElement(MemoizedGreeting, {
+      name: name,
+    })
+  );
+}
+var MemoizedGreeting = memo(Greeting);
+function Greeting(_ref) {
+  var name = _ref.name;
+  console.log("Greeting was rendered at", new Date().toLocaleTimeString());
+  var _useState5 = useState(0),
+    _useState6 = _slicedToArray(_useState5, 2),
+    count = _useState6[0],
+    setCount = _useState6[1];
+  var increment = useMemo(
+    function () {
+      return function () {
+        return setCount(count + 1);
+      };
+    },
+    [count]
+  );
+  return createElement(
+    "div",
+    null,
+    createElement("h3", null, name ? "Hello, ".concat(name, "!") : "Hello!"),
+    createElement("p", null, "Count: ", count),
+    createElement(
+      "button",
+      {
+        onClick: increment,
+      },
+      "Increment"
+    )
+  );
+}
